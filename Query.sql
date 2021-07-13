41a9cd741997e3d72faa886b9cf78bfbad814644
goggineni <87169015+goggineni@users.noreply.github.com>
2021-07-13 18:33:35 +0530

    Update Query.sql

--- Query.sql
+++ Query.sql
@@ -5,24 +5,25 @@ SELECT * FROM MealsInfo;
 SELECT* FROM MealInfo limit 1;
 
 -----LIMIT ROWS FROM TABLE---
-SELECT  * FROM VALUES limit 5
+SELECT  * from  table1 limit 5
 
 ----DELETE-----
-DELETE from VALUES
-SELECT * FROM VALUES WHERE meal_id=1438
+DELETE from table1 WHERE meal_id=1438;
+SELECT * from table1
 
 
-UPDATE VALUES
+-----UPDATE VALUES----
+UPDATE table1
 SET category = "Salad" WHERE meal_id=2307;
-SELECT* FROM VALUES;
+SELECT * from VALUES;
 
-
+---insert values
 INSERT into VALUES values(1755,"Bevarages","Thai");
 SELECT * FROM VALUES
 
 -----DROP TABLE-----
-INSERT into VALUES values(2640,"Straters","Thai");
+INSERT into value values(2640,"Straters","Thai");
 SELECT *FROM VALUES;
 
 -----SORT IN ORDER----
-SELECT * FROM VALUES ORDER BY Pizza DESC;
+SELECT * FROM VALUE ORDER BY Pizza DESC;
